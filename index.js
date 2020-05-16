cc0a4c5cbdccfe434f104e533b2a46755141dc6c
Ahmet Kütük <ack8203@gmail.com>
2020-05-16 15:34:12 +0300

    fınısh

---
+++ index.js
@@ -0,0 +1,16 @@
+function logo() {
+var x = document.getElementById("logos");
+    x.style.color="#4c4a46"
+}
+function logo2() {
+    var y = document.getElementById("logos");
+    y.style.color="white";
+}
+
+function menu(v) {
+
+    v.style.opacity="0.5";
+}
+function menu2(k) {
+    k.style.opacity="1";
+}
